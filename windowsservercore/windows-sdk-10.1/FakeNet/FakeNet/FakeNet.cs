13bb66a06f470b9188e9f91f401eead4e134eda1
Dong Xie <xied75@gmail.com>
2016-06-15 11:20:57 +0100

    added FakeNet VS solution under windows-sdk-10.1 folder

---
+++ windowsservercore/windows-sdk-10.1/FakeNet/FakeNet/FakeNet.cs
@@ -0,0 +1,25 @@
+﻿namespace FakeNet
+{
+    using System;
+    using System.Diagnostics;
+
+    class FakeNet
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+                return;
+
+            if (args[0].Trim() == "session")
+            {
+                Console.WriteLine("There are no entries in the list.\n");
+                return;
+            }
+
+            //ProcessStartInfo startInfo = new ProcessStartInfo(@"c:\windows\system32\net.exe");
+            //startInfo.Arguments = string.Join(" ", args);
+            //startInfo.UseShellExecute = false;
+            //Process.Start(startInfo);
+        }
+    }
+}
